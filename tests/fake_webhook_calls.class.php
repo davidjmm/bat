4fcdb7a278abcbe32ab96a30f825e8eceb4c0cc5
davidjmm <42723574+davidjmm@users.noreply.github.com>
2018-09-03 19:43:50 -0600

    Add files via upload

---
+++ tests/fake_webhook_calls.class.php
@@ -0,0 +1,6 @@
+$syslog_server = '127.0.0.1';
+$syslog_server_port = 514;
+
+$server = 'https : //viajeskotsu.com/jomres/api/';
+$client_id = 'vZrCYlZFzDwDBFB';
+$client_secret = 'jdXygfrQthWnwhygVnLpayAZMmVvoHMFfarLqfFsLaKpxutYQS';
